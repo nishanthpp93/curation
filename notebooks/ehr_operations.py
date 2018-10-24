8dbcd57fb488054b034c4394d5338e8371b9a0f1
Mark Velez <mav7014@nyp.org>
2018-10-23 23:49:33 -0400

    Update ehr_operations notebook
     * Most recent bucket uploads query is independent of time
     * Output to dataframe to avoid HTML (cache?) issue
     * Set project ID (some versions of datalab require)

--- notebooks/ehr_operations.py
+++ notebooks/ehr_operations.py
@@ -20,7 +20,7 @@
 #     name: python
 #     nbconvert_exporter: python
 #     pygments_lexer: ipython2
-#     version: 2.7.12
+#     version: 2.7.13
 # ---
 
 # # EHR Operations
@@ -29,34 +29,42 @@
 # %load_ext google.datalab.kernel
 
 import google.datalab.bigquery as bq
+import os
+
+app_id = os.environ.get('APPLICATION_ID')
+# %datalab project set -p $app_id
 # -
 
-# ## Most Recent Bucket Uploads Since Last Month
+# ## Most Recent Bucket Uploads
 # _Based on `storage.objects.create` log events of objects named `person.csv` archived in BigQuery_
 
 bq.Query('''
 SELECT
-  MAX(h.hpo_id) AS hpo_id,
-  MAX(m.Site_Name) AS site_name,
+  h.hpo_id AS hpo_id,
+  m.Site_Name AS site_name,
   protopayload_auditlog.authenticationInfo.principalEmail AS email,
   resource.labels.bucket_name AS bucket_name,
   MAX(SUBSTR(protopayload_auditlog.resourceName, 34)) AS resource_name,
   MAX(timestamp) AS upload_timestamp
 FROM
-  `all-of-us-rdr-prod.GcsBucketLogging.cloudaudit_googleapis_com_data_access_2018*` l
-  JOIN `aou-res-curation-prod.lookup_tables.hpo_id_bucket_name` h ON l.resource.labels.bucket_name = h.bucket_name
+  `aou-res-curation-prod.lookup_tables.hpo_id_bucket_name` h
   JOIN `aou-res-curation-prod.lookup_tables.hpo_site_id_mappings` m ON h.hpo_id = m.HPO_ID
+  LEFT JOIN `all-of-us-rdr-prod.GcsBucketLogging.cloudaudit_googleapis_com_data_access_2018*` l
+   ON l.resource.labels.bucket_name = h.bucket_name
 WHERE
-  _TABLE_SUFFIX BETWEEN '0919' AND '1019'
+  _TABLE_SUFFIX BETWEEN '0801' AND '1024'
   AND protopayload_auditlog.authenticationInfo.principalEmail IS NOT NULL
+  AND protopayload_auditlog.authenticationInfo.principalEmail <> 'aou-res-curation-prod@appspot.gserviceaccount.com'
   AND protopayload_auditlog.methodName = 'storage.objects.create'
   AND resource.labels.bucket_name LIKE 'aou%'
   AND protopayload_auditlog.resourceName LIKE '%person.csv'
 GROUP BY
-  protopayload_auditlog.authenticationInfo.principalEmail, 
-  resource.labels.bucket_name
+  h.hpo_id,
+  m.Site_Name,
+  resource.labels.bucket_name,
+  protopayload_auditlog.authenticationInfo.principalEmail
 ORDER BY MAX(timestamp) ASC
-''').execute(output_options=bq.QueryOutput.table()).result()
+''').execute(output_options=bq.QueryOutput.dataframe(use_cache=False)).result()
 
 # ## EHR Site Submission Counts
 
@@ -69,4 +77,4 @@ WHERE table_id like '%person%' AND
 NOT(table_id like '%unioned_ehr_%') AND 
 l.hpo_id <> ''
 ORDER BY Display_Order
-''').execute(output_options=bq.QueryOutput.table()).result()
+''').execute(output_options=bq.QueryOutput.dataframe(use_cache=False)).result()
